9705ca7924b6b74ce8a43c30495c2a5f835ed186
Diones Gomes <diones.zack.dz@gmail.com>
2019-11-19 23:48:09 -0300

    Adicionando classe responsável por efetuar os testes.

--- src/test/java/JPATest.java
+++ src/test/java/JPATest.java
@@ -1,39 +1,102 @@
-import dao.ArtigoDAO;
-import domain.Artigo;
+import dao.*;
+import domain.*;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import javax.inject.Inject;
+import java.util.Date;
 
-import javax.ejb.Stateless;
-import javax.persistence.GeneratedValue;
-import javax.persistence.SequenceGenerator;
 
-@Stateless
 @RunWith(Arquillian.class)
 public class JPATest {
 
     @Inject
-    ArtigoDAO artigoDao;
+    EnderecoDAO enderecoDAO;
+
+    @Inject
+    ContatoDAO contatoDAO;
+
+    @Inject
+    ParticipanteDAO participanteDAO;
+
+    @Inject
+    ArtigoDAO artigoDAO;
+
+    @Inject
+    InscricaoDAO inscricaoDAO;
+
+    @Inject
+    InscricaoefetuadaDAO inscricaoefetuadaDAO;
+
 
     @Deployment
     public static JavaArchive criarArquivoTeste() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addPackage(ArtigoDAO.class.getPackage())
-                .addPackage(Artigo.class.getPackage())
+                .addPackage(ParticipanteDAO.class.getPackage())
+                .addPackage(Participante.class.getPackage())
                 .addAsResource("META-INF/persistence.xml");
     }
     @Test
-    public void testeSalvarArtigo() {
-        Artigo artigo = new Artigo("titulo teste","orientador teste","coautores teste",
-                "modalidade teste"
-                ,"data teste","urlteste");
-        artigoDao.salvar(artigo);
+    @InSequence(1)
+    public void testeSalvarParticipante() {
+
+        Endereco endereco = new Endereco();
+        endereco.setLogradouro("Rua do Meio");
+        endereco.setBairro("Oasis");
+        endereco.setCidade("Cajazeiras");
+        endereco.setEstado("Paraíba");
+        endereco.setNumero("500");
+        endereco.setCep("58900-000");
+
+        Contato contato = new Contato();
+        contato.setTelefoneResidencial("(83) 3552-5356");
+        contato.setTelefoneComercial("(83) 3552-5356");
+        contato.setTelefoneCelular("(83) 9999-9999");
+
+        Participante participante = new Participante();
+        participante.setEndereco(endereco);
+        participante.setContato(contato);
+        participante.setNome("Salomé");
+        participante.setCPF("666.666.666-66");
+        participante.setEmail("salome@gmail.com");
+        participante.setInstituicao("IFPB");
+        participante.setNomeCracha("Salomméééé");
+        participante.setProfissao("Profesora");
+        participante.setTipoUsuario("NAO INDENTIFICADO");
+
+        Inscricaoefetuada inscricaoefetuada = new Inscricaoefetuada();
+        inscricaoefetuada.setFormaPagamento(Inscricaoefetuada.FormaPagamento.CARTAO);
+        inscricaoefetuada.setQuantidade(1);
+        inscricaoefetuada.setStatus("PAGO");
+        inscricaoefetuada.setTransacao("sei lá");
+        inscricaoefetuada.setValor(500.00);
+
+        Inscricao inscricao = new Inscricao();
+        inscricao.setDataInscricao(new Date());
+        inscricao.setPreco(200.00);
+        inscricao.setStatus("PAGO");
+        inscricao.setTipoEvento(Inscricao.TipoEvento.CONGRESSO);
+        inscricao.setTipoParticipacao("Sei lá");
+        inscricao.setParticipante(participante);
+
+        Artigo artigo = new Artigo();
+        artigo.setTitulo("Blá Blá Blá Blá");
+        artigo.setCoautores("Zé do Caibro");
+        artigo.setOrientador("Chico Chavier");
+        artigo.setData_submissao("12/12/2019");
+        artigo.setModalidade("Sei lá");
+        artigo.setUrlDownload("dadasdasdadadasd");
+        artigo.setParticipante(participante);
+
+        participanteDAO.salvar(participante);
+        inscricaoefetuadaDAO.salvar(inscricaoefetuada);
+        inscricaoDAO.salvar(inscricao);
+        artigoDAO.salvar(artigo);
 
     }
+
 }
